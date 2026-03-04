ab42a41353423d0ce85555d690c2fa5682e1ba74
Diego Beltrán <diegoramms@gmail.com>
2026-03-03 22:33:57 -0600

    Fix

--- src/main/kotlin/com/devapplab/features/cron/CronRouting.kt
+++ src/main/kotlin/com/devapplab/features/cron/CronRouting.kt
@@ -3,14 +3,13 @@ package com.devapplab.features.cron
 import com.devapplab.config.RateLimitType
 import io.ktor.server.plugins.ratelimit.*
 import io.ktor.server.routing.*
-import org.koin.ktor.ext.inject
+import org.koin.ktor.plugin.scope
 
 fun Route.cronRouting() {
-    val controller by inject<CronController>()
-
     route("/cron") {
         rateLimit(RateLimitName(RateLimitType.CRON_JOB.value)) {
             get("/ping") {
+                val controller = call.scope.get<CronController>()
                 controller.ping(call)
             }
         }
